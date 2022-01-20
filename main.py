c0ef9e0ab5ea489f16af1849112ef752f7bcae54
Aviad Azriel <aviadaz@post.bgu.ac.il>
2022-01-20 15:43:27 +0200

    Random Forest

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from sklearn.tree import DecisionTreeClassifier
 from collections import Counter
 from sklearn.preprocessing import LabelBinarizer
 from ModelSummaryTable import ModelSummaryTable
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
 
 def build_dataset():
     # https://www.phishtank.com/developer_info.php
@@ -88,6 +92,49 @@ if __name__ == "__main__":
     print("Decision Tree: Accuracy on training Data: {:.3f}".format(acc_train_tree))
     print("Decision Tree: Accuracy on test Data: {:.3f}".format(acc_test_tree))
 
+    # Multilayer Perceptrons model
+    # instantiate the model
+    mlp = MLPClassifier(alpha=0.001, hidden_layer_sizes=([100, 100, 100]))
+    # fit the model
+    mlp.fit(X_train, y_train)
+    # predicting the target value from the model for the samples
+    y_test_mlp = mlp.predict(X_test)
+    y_train_mlp = mlp.predict(X_train)
+    # computing the accuracy of the model performance
+    acc_train_mlp = accuracy_score(y_train, y_train_mlp)
+    acc_test_mlp = accuracy_score(y_test, y_test_mlp)
+    summary_table.add_row('Multilayer Perceptrons', acc_train_mlp, acc_test_mlp)
+    print("Multilayer Perceptrons: Accuracy on training Data: {:.3f}".format(acc_train_mlp))
+    print("Multilayer Perceptrons: Accuracy on test Data: {:.3f}".format(acc_test_mlp))
+
+    class_report = classification_report(y_test, y_test_mlp)
+    print(class_report)
+
+    # Random Forest
+    # instantiate the model
+    param_grid = {
+        'max_depth': [4, 8, 16],
+        'n_estimators': [1, 2, 5, 10]
+    }
+    rf = RandomForestClassifier(n_jobs=-1)
+    clf = GridSearchCV(estimator=rf, param_grid=param_grid, scoring='roc_auc')
+    # fit thr model
+    clf.fit(X_train, y_train)
+
+    rf_clf = RandomForestClassifier(max_depth=clf.best_params_['max_depth'],
+                                    n_estimators=clf.best_params_['n_estimators'])
+    rf_clf.fit(X_train, y_train)
+
+    # Making predictions
+    test_conf = rf_clf.predict(X_test)
+    train_conf = rf_clf.predict(X_train)
+
+    # Confusion Matrix and classification report
+    test_conf_matrix = confusion_matrix(y_test, test_conf)
+    class_report = classification_report(y_test, test_conf)
+    print(test_conf_matrix)
+    print(class_report)
+    summary_table.add_row('Random Forest', 1, 2, clf.best_params_['max_depth'], clf.best_params_['n_estimators'])
 
     summary_table.show()
 
